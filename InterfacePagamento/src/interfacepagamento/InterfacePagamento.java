83d20bcf34162288b6983c567c800fb8e9aa4895
Samuel e Davie <cl201278@unicamp.br>
2022-05-13 09:02:37 -0300

    a

---
+++ InterfacePagamento/src/interfacepagamento/InterfacePagamento.java
@@ -0,0 +1,9 @@
+package interfacepagamento;
+
+public class InterfacePagamento {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ InterfacePagamento/src/interfacepagamento/TipoPagamento.java
@@ -0,0 +1,9 @@
+package interfacepagamento;
+
+public interface TipoPagamento {
+    
+    public int getDiasFaturamento();
+    
+    public double getPorcentagemFinanceiraPaga();
+    
+}
