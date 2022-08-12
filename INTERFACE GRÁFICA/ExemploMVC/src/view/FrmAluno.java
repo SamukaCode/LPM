c859f34c0a3b480d89bbe889b3fb9e34299b9d44
Samuel e Davie <cl201278@unicamp.br>
2022-08-12 09:02:55 -0300

    MVC

--- INTERFACE GRÁFICA/ExemploMVC/src/view/FrmAluno.java
+++ INTERFACE GRÁFICA/ExemploMVC/src/view/FrmAluno.java
@@ -8,6 +8,7 @@ package view;
 import control.AlunoControl;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import model.Aluno;
 
 /**
@@ -40,6 +41,10 @@ public class FrmAluno extends javax.swing.JFrame {
         txtNome = new javax.swing.JTextField();
         btnCadastrar = new javax.swing.JButton();
         btnBuscarTodos = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblAlunos = new javax.swing.JTable();
+        txtProcurar = new javax.swing.JTextField();
+        btnProcurar = new javax.swing.JButton();
 
         jButton1.setText("jButton1");
 
@@ -71,25 +76,51 @@ public class FrmAluno extends javax.swing.JFrame {
             }
         });
 
+        tblAlunos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "RA", "Nome"
+            }
+        ));
+        tblAlunos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblAlunosMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tblAlunos);
+
+        txtProcurar.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+
+        btnProcurar.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        btnProcurar.setText("Procurar");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(lblRa)
+                                .addComponent(lblNome))
+                            .addGap(10, 10, 10)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(txtRa, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
+                                .addComponent(txtNome))))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnBuscarTodos)
+                        .addComponent(btnCadastrar)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnCadastrar))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lblNome)
-                            .addComponent(lblRa))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtRa, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addComponent(btnBuscarTodos)))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txtProcurar, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnProcurar))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -103,11 +134,19 @@ public class FrmAluno extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblNome)
                     .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnCadastrar)
                     .addComponent(btnBuscarTodos))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtProcurar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnProcurar)
+                        .addGap(0, 208, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         pack();
@@ -117,17 +156,40 @@ public class FrmAluno extends javax.swing.JFrame {
         String nome = this.txtNome.getText();
         int ra = Integer.parseInt(this.txtRa.getText());
         alControle.cadastrarAluno(ra, nome);
+        this.txtNome.setText("");
+        this.txtRa.setText("");
+        this.txtRa.requestFocus();
+        this.btnBuscarTodosActionPerformed(evt);
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnBuscarTodosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarTodosActionPerformed
         ArrayList<Aluno> lista = alControle.buscarTodos();
-        String result = "";
+        
+        DefaultTableModel dados = new DefaultTableModel();
+
+        dados.setNumRows(0);
+        dados.addColumn("RA");
+        dados.addColumn("Nome");
+        for(Aluno a: lista) {
+            dados.addRow(new Object[]{a.getRa(), a.getNome()});
+        }
+        
+        tblAlunos.setModel(dados);
+        
+        // CÓDIGO ANTIGO
+        /*String result = "";
         for (Aluno a: lista)
         {
             result = result.concat (a.getRa() + " " + a.getNome() + "\n");
         }
-        JOptionPane.showMessageDialog(null, result);
+        JOptionPane.showMessageDialog(null, result);*/
     }//GEN-LAST:event_btnBuscarTodosActionPerformed
+
+    private void tblAlunosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblAlunosMouseClicked
+        int linhaSelecionada = this.tblAlunos.getSelectedRow();
+        String raSelecionado = this.tblAlunos.getValueAt(linhaSelecionada,0).toString();
+        txtProcurar.setText(raSelecionado);
+    }//GEN-LAST:event_tblAlunosMouseClicked
 
     /**
      * @param args the command line arguments
@@ -167,10 +229,14 @@ public class FrmAluno extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBuscarTodos;
     private javax.swing.JButton btnCadastrar;
+    private javax.swing.JButton btnProcurar;
     private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblNome;
     private javax.swing.JLabel lblRa;
+    private javax.swing.JTable tblAlunos;
     private javax.swing.JTextField txtNome;
+    private javax.swing.JTextField txtProcurar;
     private javax.swing.JTextField txtRa;
     // End of variables declaration//GEN-END:variables
 }
